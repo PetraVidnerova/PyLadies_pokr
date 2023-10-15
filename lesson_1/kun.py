a712ca740f5604f60aae65ad110fc56075537208
Petra Vidnerová <petra.vidnerova@gmail.com>
2023-10-15 18:10:55 +0200

    without objects

--- lesson_1/kun.py
+++ lesson_1/kun.py
@@ -1,131 +1,78 @@
 from utils import yes_or_no
 
+SIZE = 8
+START = (3, 2)
+GOAL = (7, 6)
 
-class Coordinate():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+horse_moves = [
+    (2, 1),
+    (2, -1),
+    (-2, 1),
+    (-2, -1),
+    (1, 2),
+    (-1, 2),
+    (1, -2),
+    (-1, -2)
+]
 
-    def __eq__(self, b):
-        if type(b) == tuple:
-            return self.x == b[0] and self.y == b[1]
-        else:
-            return self.x == b.x and self.y == b.y
+
+def horse_jump(horse):
         
-    def copy(self):
-        return Coordinate(self.x, self.y)
+    for x, y in horse_moves:
+        new_horse = (
+            horse[0] + x,
+            horse[1] + y
+        )
+        yield new_horse
 
-class HorsePath():
-
-    horse_moves = [
-        Coordinate(2, 1),
-        Coordinate(2, -1),
-        Coordinate(-2, 1),
-        Coordinate(-2, -1),
-        Coordinate(1, 2),
-        Coordinate(-1, 2),
-        Coordinate(1, -2),
-        Coordinate(-1, -2)
-    ]
-
-    def __init__(self):
-        self.path = []
-
-    def copy(self):
-        new_path = HorsePath()
-        new_path.path = self.path.copy()
-        return new_path
-
-    def len(self):
-        return len(self.path)
+def valid_horse(horse):
+    return 0 <= horse[0] < SIZE and 0 <= horse[1] < SIZE
         
-    def append(self, pos):
-        if pos in self.path:
-            return False
-        self.path.append(pos.copy())
-        return True
-    
-    def get_pos(self):
-        return self.path[-1]
-
-    def leads_to_goal(self, goal):
-        return self.path[-1] == goal
-
-    def jump(self):
-        new_horse = Coordinate(0, 0)
-        
-        for move in self.horse_moves:
-            new_horse.x = self.get_pos().x + move.x
-            new_horse.y = self.get_pos().y + move.y
-            yield new_horse
-
-        
-class Board():
-
-    def __init__(self, horse, goal, size=8):
-        if not (0 <= horse.x < size  and 0 <= horse.y < size):
-            raise ValueError("Horse out of board.")
-        if not (0 <= goal.x < size and 0 <= goal.y < size):
-            raise ValueError("Goal out of board.")
-
-        self.horse = horse
-        self.goal = goal
-        self.size = size
-
-    def validate_horse(self, horse):
-        return 0 <= horse.x < self.size and 0 <= horse.y < self.size
-        
-    def print_me(self):
-        for x in range(self.size):
-            for y in range(self.size):
-                if (x, y) == self.horse:
-                    print("\N{Horse}", end="")
-                elif (x, y) == self.goal:
-                    print("\N{Crown}", end="")
+def print_board(size, horse, goal):
+    for x in range(size):
+        for y in range(size):
+            if (x, y) == horse:
+                print("\N{Horse}", end="")
+            elif (x, y) == goal:
+                print("\N{Crown}", end="")
+            else:
+                remainder = 1 if y % 2 == 0 else 0
+                if x % 2 == remainder:
+                    print("\N{Black Large Square}", end="")
                 else:
-                    remainder = 1 if y % 2 == 0 else 0
-                    if x % 2 == remainder:
-                        print("\N{Black Large Square}", end="")
-                    else:
-                        print("\N{White Large Square}", end="")
-            print()
+                    print("\N{White Large Square}", end="")
+        print()
 
-    def animate_path(self, path):
-        for horse in path.path:
-            self.horse = horse
-            self.print_me()
-            if horse != self.goal:
-                input(" Press Enter ...")
+def animate_path(path):
+    for horse in path:
+        print_board(SIZE, horse, GOAL)
+        if horse != GOAL:
+            input(" Stiskni Enter ...")
 
 def main():
-    horse = Coordinate(3, 2)
-    goal = Coordinate(7,6)
-    board = Board(horse, goal)
 
     path_repo = []
-
-    # path of length 0
-    path = HorsePath()
-    path.append(horse)
+    path = [START]
     path_repo.append(path)
-
 
     while path_repo:
         path = path_repo.pop(0)
 
-        print(f"Cesta délky {path.len()}. Zkusíme skočit dál.") 
+        print(f"Cesta délky {len(path)}. Zkusíme skočit dál.") 
         
-        for new_horse in path.jump():
-            if not board.validate_horse(new_horse):
+        for new_horse in horse_jump(path[-1]):
+            if not valid_horse(new_horse):
                 # cesta by vedla mimo hraci pole
                 continue
             new_path = path.copy()
-            if not new_path.append(new_horse):
+            if new_horse in new_path: 
                 # cesta by se zacyklila
                 continue
-            if new_path.leads_to_goal(goal):
+            else:
+                new_path.append(new_horse)
+            if new_path[-1] == GOAL:
                 print("HURA HURA")
-                board.animate_path(new_path)
+                animate_path(new_path)
                 if not yes_or_no("Chceš hledat jinou cestu? "):
                     return
             else:
