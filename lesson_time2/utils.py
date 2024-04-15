01bd1e3e637290cbcf87da30b37874c849217b69
Petra Vidnerová <petra.vidnerova@gmail.com>
2024-04-15 16:26:48 +0200

    change of repeat

--- lesson_time2/utils.py
+++ lesson_time2/utils.py
@@ -10,12 +10,13 @@ def create_random_list(n):
 def test_time(function, repeat=1):
     times = {}
     for length in range(1000, 10000, 1000):
-        my_list = create_random_list(length)
-        start = time.time()
+        t = 0
         for _ in range(repeat):
+            my_list = create_random_list(length)
+            start = time.time()
             result = function(my_list)
-        end = time.time()
-        t = end-start 
+            end = time.time()
+            t += end-start 
         print(f"Time for length {length} is {t:.6f} seconds")
         times[length] = t
     print()
